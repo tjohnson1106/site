d799bf118b88952276e73c861f70095154120323
Thomas Johnson <tjohnson1106@gmail.com>
2017-11-14 17:40:11 -0500

    added HashRouter and path props

--- src/Main.js
+++ src/Main.js
@@ -1,23 +1,33 @@
 import React, { Component } from "react";
+import { Route, NavLink, HashRouter } from "react-router-dom";
+import Home from "./Home";
+import Blog from "./Blog";
+import Contact from "./Contact";
 
 class Main extends Component {
   render() {
     return (
-      <div>
-        <h1> Bracket Factory </h1>
-        <ul className="header">
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/blog">Blog</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-        <div className="content" />
-      </div>
+      <HashRouter>
+        <div>
+          <h1> Bracket Factory </h1>
+          <ul className="header">
+            <li>
+              <NavLink href="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink href="/blog">Blog</NavLink>
+            </li>
+            <li>
+              <NavLink href="/contact">Contact</NavLink>
+            </li>
+          </ul>
+          <div className="content">
+            <Route path="/" component={Home} />
+            <Route path="/blog" component={Blog} />
+            <Route path="/contact" component={Contact} />
+          </div>
+        </div>
+      </HashRouter>
     );
   }
 }
